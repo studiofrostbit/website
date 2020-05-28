3bd290fe6c1d1c2e5a3613022499391d09bf7c17
teevik <teemu.vikoren@gmail.com>
2020-05-28 13:21:05 +0200

    Updates

--- component---src-pages-404-tsx-2f17e77a8846ec81227d.js
+++ component---src-pages-404-tsx-2f17e77a8846ec81227d.js
@@ -1,1 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"i6+/":function(e,t,n){"use strict";n.r(t),n.d(t,"default",(function(){return r}));var a=n("q1tI"),u=n.n(a),l=n("9Dj+"),o=n("H8eV");function r(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-2f17e77a8846ec81227d.js.map
